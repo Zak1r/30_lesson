a92b3daebf537fded5a671d02774e137e15b926e
Zak1r <94119532+Zak1r@users.noreply.github.com>
2022-02-28 20:10:48 +0600

    121 taask done

---
+++ index.py
@@ -0,0 +1,30 @@
+# 121
+
+# from random import randint, randrange
+
+# print(randrange(1, 49 + 1))
+# print(randint(1, 49))
+
+
+# ! КОД
+
+# from random import randint
+
+# MIN_NUM = 1
+# MAX_NUM = 49
+# NUM_NUMS = 6
+
+# ticket_nums = []
+
+# for i in range(NUM_NUMS):
+#     rand = randint(MIN_NUM, MAX_NUM)
+#     while rand in ticket_nums:
+#         rand = randint(MIN_NUM, MAX_NUM)
+#     ticket_nums.append(rand)
+
+# ticket_nums.sort()
+# for tn in ticket_nums:
+#     print(tn, end=' ')
+
+
+# 123
